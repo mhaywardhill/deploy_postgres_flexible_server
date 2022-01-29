d885729500fdd787f90e134b033916b82803596d
mhh <haywardhill@gmail.com>
2022-01-29 12:51:35 +0000

    add log analytics module

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -28,40 +28,47 @@ module "nsg" {
   vm_subnet_id        = module.network.vm_subnet_id
   project             = var.project_name
   my_public_ip        = var.my_public_ip
-  environment_name     = var.environment_name
+  environment_name    = var.environment_name
   depends_on = [module.network]
 }
 
 module "publicip" {
-  source           = "../../modules/publicip"
-  location         = var.location
-  resource_group   = module.resource_group.resource_group_name
-  project          = var.project_name
-  environment_name     = var.environment_name
+  source              = "../../modules/publicip"
+  location            = var.location
+  resource_group      = module.resource_group.resource_group_name
+  project             = var.project_name
+  environment_name    = var.environment_name
   depends_on = [module.network]
 }
 
 module "vm" {
-  source           = "../../modules/vm"
-  location         = var.location
-  resource_group   = module.resource_group.resource_group_name
-  subnet_id        = module.network.vm_subnet_id
-  public_ip_address_id = module.publicip.public_ip_address_id
-  project          = var.project_name
-  vm_username      = var.vm_username
-  environment_name     = var.environment_name
+  source                 = "../../modules/vm"
+  location              = var.location
+  resource_group        = module.resource_group.resource_group_name
+  subnet_id             = module.network.vm_subnet_id
+  public_ip_address_id  = module.publicip.public_ip_address_id
+  project               = var.project_name
+  vm_username           = var.vm_username
+  environment_name      = var.environment_name
   depends_on = [module.nsg]
 }
 
 module "postgresql" {
-  source               = "../../modules/postgresql"
-  location             = var.location
-  resource_group       = module.resource_group.resource_group_name
-  server_name	         = "${var.project_name}-pgfs-${var.environment_name}"
-  db_username          = var.db_username
-  db_password          = var.db_password
-  project              = var.project_name
-  delegated_subnet_id  = module.network.postgres_subnet_id
-  private_dns_zone_id  = module.network.dns_zone_id
+  source                = "../../modules/postgresql"
+  location              = var.location
+  resource_group        = module.resource_group.resource_group_name
+  server_name	          = "${var.project_name}-pgfs-${var.environment_name}"
+  db_username           = var.db_username
+  db_password           = var.db_password
+  project               = var.project_name
+  delegated_subnet_id   = module.network.postgres_subnet_id
+  private_dns_zone_id   = module.network.dns_zone_id
   depends_on = [module.nsg]
+}
+module "log_analytics" {
+  source                = "../../modules/log_analytics"
+  location              = var.location
+  resource_group        = module.resource_group.resource_group_name
+  workspace_name        = "${var.project_name}-logs-${var.environment_name}"
+  project               = var.project_name
 }
